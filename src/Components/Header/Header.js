728a881e5b7d9f5ec42916ce6e8e428879fc7127
eddipietro <erikadipietro7@gmail.com>
2022-06-15 16:46:17 -0300

    menu

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,23 @@
+import logo from "../../assest/logo.jpg"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { cyan } from '@mui/material/colors';
+import "../Header.css"
+
+const Header = () =>{
+    return(
+        <header className="header">
+            <img src={logo} alt=""/>
+            
+            <nav>
+                <a href="">NOSOTROS</a>
+                <a href="">CONTACTO </a>
+                <a href="">PRODUCTOS</a>
+
+                
+            </nav>
+            <ShoppingCartIcon sx={{ color: cyan[500] }} />
+        </header>
+
+    )
+}
+export default Header
